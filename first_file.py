728ed229de147652a25b1f9b6faaed21ee0f7fb7
Charles_Seng_Naw <jerome.jatnaw2016@gmail.com>
2026-09-13 21:44:26 +0700

    This is my first commit.

---
+++ first_file.py
@@ -0,0 +1,8 @@
+# This is the first file and my first comit.
+print("Hello, World!")
+print("Hello, Again!")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
